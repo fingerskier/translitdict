e10f0f86a15c1ef1eff05ac0e57f82df768338f1
fingerskier <fingerskier@gmail.com>
2024-10-19 14:01:26 -0500

    GH Pages

--- src/com/NestedListEditor.js
+++ src/com/NestedListEditor.js
@@ -15,15 +15,15 @@ export default function NestedListEditor({ list, onAddItem, onRemoveItem, onUpda
         <li key={index}>
           <div>
             <button onClick={() => toggleCollapse(index)}>
-              {collapsed[index] ? '➕' : '➖'}
+              {collapsed[index] ? '🔽': '🔼' }
             </button>
             <input 
               type="text" 
               value={item.name} 
               onChange={(e) => onUpdateItem(e, index, path)} 
             />
-            <button onClick={() => onAddItem(index, path)}>Add Child</button>
-            <button onClick={() => onRemoveItem(index, path)}>Remove</button>
+            <button onClick={() => onAddItem(index, path)}>➕</button>
+            <button onClick={() => onRemoveItem(index, path)}>❌</button>
           </div>
           {!collapsed[index] && item.children.length > 0 && (
             <NestedListEditor
